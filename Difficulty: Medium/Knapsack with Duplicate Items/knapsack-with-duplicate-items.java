d63ebd804f4252d84dac3d721e9a00ac8c0ec807
Ravikant Turi <145963203+ravikant-turi@users.noreply.github.com>
2024-12-04 11:24:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+            int W = Integer.parseInt(in.readLine().trim());
+            String line2[] = in.readLine().trim().split(" ");
+            int N = line2.length;
+            int val[] = new int[N];
+            for (int i = 0; i < N; i++) val[i] = Integer.parseInt(line2[i]);
+            String line3[] = in.readLine().trim().split(" ");
+            int wt[] = new int[N];
+            for (int i = 0; i < N; i++) wt[i] = Integer.parseInt(line3[i]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.knapSack(val, wt, W));
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int knapSack(int val[], int wt[], int capacity) {
+        
+        
+        // code here
+        int W=capacity;
+        int n=val.length;
+         int [][]dp=new int [n+1][W+1];
+         
+        //Intilization
+        for(int i=0;i<n+1;i++){
+            dp[i][0]=0;
+        }
+        
+         for(int i=0;i<W+1;i++){
+            dp[0][i]=0;
+        }
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<W+1;j++){
+                if(wt[i-1]<=j){
+                    dp[i][j]= Math.max(val[i-1]+dp[i][j-wt[i-1]] ,dp[i-1][j]);
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][W];
+        
+        
+        
+    }
+}
